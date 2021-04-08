05d0788bc8a4cc9164607bc41b7618a95ab45258
Carlos <carlostena@centroafuera.es>
2021-04-08 13:21:52 +0200

    Corrutinas mejoradas

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,35 @@
 import kotlinx.coroutines.*
 import kotlin.random.Random
 
+var mostrarLogsDelBlocking = false
+
 fun main() {
+  /*
     println("Empezamos aquí")
 
-    /*for (i in 0..5) {
-        GlobalScope.launch {
-            funcionEnCorrutina(i)
-        }
-    }*/
+    println("Entramos")
+    GlobalScope.launch {
+        // Este es el ejemplo más básico de una corrutina
+        mostrarLog(1, this)
+        funcionEnCorrutina(1)
+    }
+    println("Salimos")
+
+    // Ponemos este sleeep para que la corrutina anterior no se entremezcle con las siguiente corrutinas
+    Thread.sleep(3000)
+
     println("Entramos")
     runBlocking {
-        this.launch {             
+        mostrarLog(2, this)
+
+        // this es el scope en el que se va a ejecutar las 2 corrutinas y el runblocking va a forzar a que terminen antes de poder salir
+        this.launch {
+            mostrarLog(3, this)
+            funcionEnCorrutina(98)
+        }
+        this.launch {
+            mostrarLog(4, this)
+
             funcionEnCorrutina(99)
         }
     }
@@ -19,30 +37,70 @@ fun main() {
 
     println("Entramos")
     runBlocking {
+        mostrarLog(5, this)
+        // this es el scope en el que no se va a ejecutar  nada, por lo que las 2 corrutinas y el runblocking no van a tener un efecto real
         GlobalScope.launch {
+            mostrarLog(6, this)
             funcionEnCorrutina(100)
+        }
+        GlobalScope.launch {
+            mostrarLog(7, this)
+            funcionEnCorrutina(101)
         }
     }
     println("Salimos")
-    println("Esperamos un rato")
-    Thread.sleep(200000)
-    println("Terminamos aquí")
 
-
-    var b = funcion()
-    println("Hola")
+    // Ponemos este sleeep para que las corrutinas anteriores no se entremezclen con las siguiente corrutinas
+    Thread.sleep(3000)
+*/
     GlobalScope.launch {
-
-
-    if (b.await()) {
-
+        funcionAsyncNormal()
     }
+    Thread.sleep(3000)
+
+    GlobalScope.launch {
+        funcionAsyncLazy()
     }
+    Thread.sleep(5000)
 }
-private fun funcion(): Deferred<Boolean> {
-    return GlobalScope.async {
-        delay(10000000)
-        true }
+
+
+private suspend fun funcionAsyncNormal(){
+    println("Estoy en la función")
+    val resultDeferred = GlobalScope.async {
+        val resultInCoroutine = Random.nextInt()
+        val tiempoDeDormir = 2000L
+        println("voy a devolver un $resultInCoroutine en $tiempoDeDormir")
+        delay(tiempoDeDormir)
+        println("He terminado el async")
+        resultInCoroutine
+    }
+    println("Voy a hacer el await")
+    // El await esperará a que el GlobalSncope.async {} termine de ejecutarse. Si el aync ya hubiera terminado,
+    // entonces el await no esperaría, xq ya tiene el resultado
+    println(resultDeferred.await())
+    println("Voy a salir del await")
+}
+
+private suspend fun funcionAsyncLazy(){
+    println("Estoy en la función")
+    val resultDeferred = GlobalScope.async(start = CoroutineStart.LAZY) {
+        val resultInCoroutine = Random.nextInt()
+        val tiempoDeDormir = 2000L
+        println("voy a devolver un $resultInCoroutine en $tiempoDeDormir")
+        delay(tiempoDeDormir)
+        println("He terminado el async")
+        resultInCoroutine
+    }
+    println("Voy a hacer el await")
+    println(resultDeferred.await())
+    println("Voy a salir del await")
+}
+
+
+private fun mostrarLog(identificador : Int, scope : CoroutineScope){
+    if (mostrarLogsDelBlocking) println("$identificador - ${scope.coroutineContext}")
+
 }
 
 private suspend fun funcionEnCorrutina(i : Int){
