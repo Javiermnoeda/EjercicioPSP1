37c1511e72d791bebf90753cd5b19740782b30d2
Javier <jmnoeda2000@gmail.com>
2021-04-09 14:16:11 +0200

    Ejercicio Corrutinas PSP

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -16,8 +16,9 @@ fun main(){
               delay(5000)
               println("Sigo vivo")
           }
-          println("He terminado")
+
       }
+      println("He terminado")
   }
 
     Thread.sleep(15000) // El hilo donde se esta ejecutando algo se para. se usa para que el hilo que se ejecuta fuera de la corrutina no lo haga
